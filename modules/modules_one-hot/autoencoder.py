8bae1a82ad82f2184f9197323389984786311543
Komolov Aleksandr <askomolov@mail.ru>
2025-05-25 08:33:34 +0300

    Rewrite class def in autoencoder.py

--- modules/modules_one-hot/autoencoder.py
+++ modules/modules_one-hot/autoencoder.py
@@ -3,15 +3,38 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
-class Autoencoder(nn.Module):
-    def __init__(self, input_dims=399):
+class AutoencoderCdrOH(nn.Module):
+    def __init__(self):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(in_features=input_dims, out_features=latent_dims),
+            nn.Linear(in_features=399, out_features=latent_dims),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(in_features=latent_dims, out_features=input_dims),
+            nn.Linear(in_features=latent_dims, out_features=399),
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    def encoding(self, x):
+        encoded = self.encoder(x)
+        return encoded
+
+    def decoding(self, encoded):
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+class AutoencoderEpitopeOH(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=420, out_features=latent_dims),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=latent_dims, out_features=420),
         )
 
     def forward(self, x):
