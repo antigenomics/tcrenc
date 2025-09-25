db295392a14329498571f967f1783ffc90085413
Komolov Aleksandr <askomolov@mail.ru>
2025-09-25 22:25:36 +0300

    Add model loading function

---
+++ tcrenc/models/models_list.py
@@ -0,0 +1,45 @@
+from tcrenc.utils.basic import read_config
+
+
+def load_model(args, script_type):
+
+    if script_type != 'train' or (not args.encoder_train and not args.decoder_train):
+        if args.embed_type == 'onehot':
+            model_config = read_config('./tcrenc/configs/config_onehot.yaml', script_type=script_type)
+            from models.autoencoder_onehot.autoencoder_onehot import Autoencoder_onehot as Model
+
+        elif args.embed_type == 'kidera':
+            model_config = read_config('./tcrenc/configs/config_kidera.yaml', script_type=script_type)
+            from models.autoencoder_kidera.autoencoder_kidera import Autoencoder_kidera as Model
+
+        elif args.embed_type == 'atchley':
+            model_config = read_config('./tcrenc/configs/config_atchley.yaml', script_type=script_type)
+            # from models.autoencoder_atchley.autoencoder_atchley import Autoencoder_atchley as Model
+    else:
+        if args.encoder_train:
+            if args.embed_type == 'onehot':
+                model_config = read_config('./tcrenc/configs/config_onehot.yaml', script_type=script_type)
+                from models.autoencoder_onehot.encoder_onehot import Encoder_onehot as Model
+
+            elif args.embed_type == 'kidera':
+                model_config = read_config('./tcrenc/configs/config_kidera.yaml', script_type=script_type)
+                from models.autoencoder_kidera.encoder_kidera import Encoder_kidera as Model
+
+            elif args.embed_type == 'atchley':
+                model_config = read_config('./tcrenc/configs/config_atchley.yaml', script_type=script_type)
+                # from models.autoencoder_atchley.encoder_atchley import Encoder_atchley as Model
+
+        elif args.decoder_train:
+            if args.embed_type == 'onehot':
+                model_config = read_config('./tcrenc/configs/config_onehot.yaml', script_type=script_type)
+                from models.autoencoder_onehot.decoder_onehot import Decoder_onehot as Model
+
+            elif args.embed_type == 'kidera':
+                model_config = read_config('./tcrenc/configs/config_kidera.yaml', script_type=script_type)
+                from models.autoencoder_kidera.decoder_kidera import Decoder_kidera as Model
+
+            elif args.embed_type == 'atchley':
+                model_config = read_config('./tcrenc/configs/config_atchley.yaml', script_type=script_type)
+                # from models.autoencoder_atchley.decoder_atchley import Decoder_atchley as Model
+
+    return Model, model_config
