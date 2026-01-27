f06239fa3b3f04780e86bef6157d3f8cdd8fa25c
Komolov Aleksandr <askomolov@mail.ru>
2026-01-27 18:12:51 +0300

    Add dockstrings for abstract classes

--- tcrenc/models/decoder.py
+++ tcrenc/models/decoder.py
@@ -1,34 +1,81 @@
 from abc import abstractmethod
+import pandas as pd
 
+import torch
 from torch.nn import Module
 
 
 class Decoder(Module):
     @abstractmethod
-    def __init__(self,):
+    def __init__(self,
+                 config: dict,
+                 seq_type: str,
+                 device: torch.device):
         super(Decoder, self).__init__()
+        """
+        Initialize the base Decoder.
+
+        Args:
+            config (dict): Configuration dictionary with model parameters.
+            seq_type (str): Type of sequence to process.
+                Options:
+                    - "antigene_epitope"
+                    - "cdr3"
+            device (torch.device): Target device for computation.
+                Required even if the implementation does not use PyTorch.
+        """
         pass
 
     @abstractmethod
-    def forward(self,):
+    def weight_load(self):
+        """
+        Abstract method for loading model weights.
+
+        For non-NN models, this method can be left empty but must still be implemented.
+        """
         pass
 
     @abstractmethod
-    def weight_load(self,):
+    def make_seq_from_embeddings(self,
+                                 input_embds: pd.DataFrame) -> pd.DataFrame:
+        """
+        Reconstruct sequences from input embeddings.
+
+        Args:
+            input_embds (pd.DataFrame): A Pandas DataFrame containing embeddings.
+                Each row should represent a single sequence embedding.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing reconstructed sequences.
+        """
         pass
 
     @abstractmethod
-    def make_seq_from_embeddings(self,):
+    def model_train(self,
+                    train_data: pd.DataFrame,
+                    criterion,
+                    test_data: pd.DataFrame = None) -> None:
+        """
+        Train the model using the provided training data.
+
+        Args:
+            train_data (pd.DataFrame): DataFrame containing input training data.
+                For decoder models there are sequences and embeddings in input.
+            criterion: Loss function or evaluation criterion used for training.
+            test_data (pd.DataFrame, optional): Optional DataFrame with test or validation
+                data for monitoring training performance. Defaults to None.
+
+        Returns:
+            None
+        """
         pass
 
     @abstractmethod
-    def input_data_process(self,):
-        pass
+    def save_model(self):
+        """
+        Save the current model.
 
-    @abstractmethod
-    def model_train(self,):
-        pass
-
-    @abstractmethod
-    def save_model(self,):
+        Returns:
+            None
+        """
         pass
--- tcrenc/models/encoder.py
+++ tcrenc/models/encoder.py
@@ -1,34 +1,82 @@
 from abc import abstractmethod
+import pandas as pd
 
+import torch
 from torch.nn import Module
 
 
 class Encoder(Module):
     @abstractmethod
-    def __init__(self,):
+    def __init__(self,
+                 config: dict,
+                 seq_type: str,
+                 device: torch.device):
+        """
+        Initialize the base Encoder.
+
+        Args:
+            config (dict): Configuration dictionary with model parameters.
+            seq_type (str): Type of sequence to process.
+                Options:
+                    - "antigene_epitope"
+                    - "cdr3"
+            device (torch.device): Target device for computation.
+                Required even if the implementation does not use PyTorch.
+        """
         super(Encoder, self).__init__()
         pass
 
     @abstractmethod
-    def forward(self,):
+    def weight_load(self):
+        """
+        Abstract method for loading model weights.
+
+        For non-NN models, this method can be left empty but must still be implemented.
+        """
         pass
 
     @abstractmethod
-    def weight_load(self,):
+    def make_embeddings_from_seq(self,
+                                 input_data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Create embeddings from input biological sequences.
+
+        Args:
+            input_data (pd.DataFrame): A Pandas DataFrame containing a single column
+                with one type of sequences (either `cdr3` or `antigen_epitope`).
+
+        Returns:
+            pd.DataFrame: A DataFrame containing generated embeddings.
+                The original input sequences are included in the last column.
+        """
         pass
 
     @abstractmethod
-    def make_embeddings_from_seq(self,):
+    def model_train(self,
+                    train_data: pd.DataFrame,
+                    criterion,
+                    test_data: pd.DataFrame = None) -> None:
+        """
+        Train the model using the provided training data.
+
+        Args:
+            train_data (pd.DataFrame): DataFrame containing input training data.
+                For encoder models there are sequences and embeddings in input.
+            criterion: Loss function or evaluation criterion used for training.
+            test_data (pd.DataFrame, optional): Optional DataFrame with test or validation
+                data for monitoring training performance. Defaults to None.
+
+        Returns:
+            None
+        """
         pass
 
     @abstractmethod
-    def input_data_process(self,):
-        pass
+    def save_model(self):
+        """
+        Save the current model.
 
-    @abstractmethod
-    def model_train(self,):
-        pass
-
-    @abstractmethod
-    def save_model(self,):
+        Returns:
+            None
+        """
         pass
